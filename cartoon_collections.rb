528cd2049bca33d00c663a4f49fce2dd1e07b44f
Tony Nassif <tonynassif@gmail.com>
2017-05-29 19:00:57 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,10 +7,19 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any? { |i| i.length > 4} 
+  array.any? { |i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.include?("cheddar")
+    return "cheddar"
+  elsif array.include?("gouda")
+    return "gouda"
+  elsif array.include?("camembert")
+    return "camembert"
+  else
+    nil
+  end
 end

76b290b1980fcfa6ae27398e25a120ae2665755a
Lashaunna Samuels <shaunna191@hotmail.com>
2017-08-01 17:16:19 -0400

    Minimized my method by using #collect on each array element

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,11 +3,8 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index { |value, index| puts "#{index + 1}.#{value}" }
 end
 
-def summon_captain_planet(array)
-  array.each do |x|
-      x.capitalize!
-      puts x + "!"
-  end
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect { |x| x.capitalize + "!" }
 end
 
 def long_planeteer_calls# code an argument here

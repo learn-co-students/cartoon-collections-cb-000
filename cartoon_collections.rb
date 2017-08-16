44b335aca96f919e64882ae1f6df27ce4768ae06
Muhammad Zeeshan Shafqat <101041699@student.swin.edu.au>
2017-08-16 22:34:25 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarfs)
+  dwarfs.each_with_index do |dwarf_name, index|
+    puts "#{index + 1}.*#{dwarf_name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteers)
+  planeteers.collect { |name|
+    name = name + "!"
+    name.capitalize
+  }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(call)
+  call.any? { |calls| calls.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  found = cheese.detect { |c| c == "cheddar"}
+  return found
 end

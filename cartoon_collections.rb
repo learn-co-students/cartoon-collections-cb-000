23aa390771780b76dd49e965de42523ae6b3ec24
Marina Paskal <paskalm@rogers.com>
2017-10-14 22:57:42 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  i = 0
+  array.each_with_index { |name, i| puts "#{i + 1}. #{name}" }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.map { |name| name.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
+  common = []
   cheese_types = ["cheddar", "gouda", "camembert"]
+  common = array & cheese_types
+     if common.empty?
+      return nil
+    else
+      return common[0]
+    end
 end

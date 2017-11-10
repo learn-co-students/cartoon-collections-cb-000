e34e938467a5371ac9660caa50c00dbcc2e711de
Heather Christman <efl7a@yahoo.de>
2017-11-10 15:35:33 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(collection)
+  collection.each_with_index do |element, index|
+    puts "#{index+1} #{element}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(collection)
+  capitalized_collection = collection.map do|element|
+    "#{element.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(collection)
+  collection.any? do |element|
+    element.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(collection)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  collection.each do |element|
+    cheese_types.each do |cheese|
+      if (element == cheese)
+        return(element)
+      end
+    end
+  end
+  nil
 end

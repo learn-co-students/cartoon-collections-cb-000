e2251705e715aa4d5a97123c068096363e612291
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-25 23:47:23 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,22 +7,20 @@ end
 
 def summon_captain_planet(summons)
   summons.map {|summon| summon.capitalize + "!"}
-
 end
 
 def long_planeteer_calls(array)
   array.any? { |word| word.length > 4 }
 end
 
-
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
   cheese_types.any? do |i|
-  if  array.include?(i)
-  #if array.include?(cheese_types)
-    return i
-  else
-    return nil
-  end
+    if  array.include?(i)
+      return i
+    else
+      return nil
+    end
   end
 end

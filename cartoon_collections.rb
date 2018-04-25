5fe651dfad8323332134b68abee57778ee188c33
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-25 22:47:10 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,13 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index do |item, index|
+    index += 1
+    puts "#{index} #{item}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.first.upcase
+
 end
 
 def long_planeteer_calls# code an argument here
